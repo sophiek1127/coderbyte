71cae1a1016d3963a3a1f57eb5e37de5e87ebd31
sophiek1127 <sophiek1127@gmail.com>
2015-08-02 13:53:54 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,31 @@
+def SimpleAdding(num)
+
+  a=0
+  (1..num).each {|p| a+=p}
+  
+  return a 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
